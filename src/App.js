8167dfcf61976ad589305ccffec0e0c515fba1f7
Hannah Redler <hannah.redler@glovoapp.com>
2021-07-31 16:31:51 +0200

    add about me page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import aboutMePhoto from './assets/images/about-me.jpeg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="nav-item">About me</div>
+        <div className="nav-item">Articles</div>
+        <div className="nav-item">Projects</div>
       </header>
+      <div className="container">
+        <div className="page about-me">
+          <div className="page-title">
+            <div className="page-title-text">
+              <h1>{"Hannah Redler"}</h1>
+              <h2>{"Software Engineer"}</h2>
+            </div>
+            <div className="page-title-image">
+              <img src={aboutMePhoto} height="200"/>
+            </div>
+          </div>
+          <div className="page-body">
+            <p>{ "Experience in frontend and backend, agile methodologies and startup environments. Lover of finding simple solutions to complex problems." }</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
