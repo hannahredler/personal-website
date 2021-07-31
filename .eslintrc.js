66f1e4ef64681bc9ea93370a281e10d1f787f9f1
Hannah Redler <hannah.redler@glovoapp.com>
2021-07-31 16:31:14 +0200

    linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
+        "amd": true,
+        "node": true,
         "es2021": true
     },
     "extends": [
@@ -17,6 +19,10 @@ module.exports = {
     "plugins": [
         "react"
     ],
-    "rules": {
-    }
+    rules: {
+        // suppress errors for missing 'import React' in files
+        "react/react-in-jsx-scope": "off",
+        // allow jsx syntax in js files (for next.js project)
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
+      }
 };
